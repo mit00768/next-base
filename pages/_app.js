6ee59eee77fdc8c319dcd37e751b532a415c75b1
Howard <mit00768@gmail.com>
2024-07-26 12:01:30 +0800

    726

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
+// 3. 最外(上)元件階層包裹提供者元件，讓⽗⺟元件可以提供它
+import { AuthProvider } from '@/hooks/use-auth'
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
   const getLayout = Component.getLayout || ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
 }
