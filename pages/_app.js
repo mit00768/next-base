49dbca3397aebeb6974ac213c4ab7e07161e8f22
Howard <mit00768@gmail.com>
2024-07-29 15:13:01 +0800

    726

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+
 // 3. 最外(上)元件階層包裹提供者元件，讓⽗⺟元件可以提供它
 import { AuthProvider } from '@/hooks/use-auth'
+// 套用CartProvider，才能使用useCart獲得CartContext中的值
+import { CartProvider } from '@/hooks/use-cart'
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
   const getLayout = Component.getLayout || ((page) => page)
 
-  return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
+  return (
+    <AuthProvider>
+      <CartProvider>{getLayout(<Component {...pageProps} />)}</CartProvider>
+    </AuthProvider>
+  )
 }
