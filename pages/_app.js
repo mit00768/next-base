44bd59fc643a910733c948f78fcd9964a374e823
Howard <mit00768@gmail.com>
2024-07-31 13:43:57 +0800

    731

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-
 // 3. 最外(上)元件階層包裹提供者元件，讓⽗⺟元件可以提供它
 import { AuthProvider } from '@/hooks/use-auth'
 // 套用CartProvider，才能使用useCart獲得CartContext中的值
 import { CartProvider } from '@/hooks/use-cart'
+import NextTopLoader from 'nextjs-toploader'
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
@@ -10,7 +10,10 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <AuthProvider>
-      <CartProvider>{getLayout(<Component {...pageProps} />)}</CartProvider>
+      <CartProvider>
+        <NextTopLoader color="purple" />
+        {getLayout(<Component {...pageProps} />)}
+      </CartProvider>
     </AuthProvider>
   )
 }
